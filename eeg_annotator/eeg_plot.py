0fc4e7d569a707be4378483d64506adfeade0430
Fraol Gelana <fraolgela@gmail.com>
2024-03-01 10:48:03 +0300

    get sfreq , .edf support for varyying channel names

--- eeg_annotator/eeg_plot.py
+++ eeg_annotator/eeg_plot.py
@@ -22,7 +22,7 @@ class EEGPlot:
         mean_pwr = np.mean(pwr)
         std_pwr = np.std(pwr)
         self.scale_factor = (mean_pwr + std_pwr) * 3
-        print(f"{self.scale_factor=}")
+        print(f"SCALE FACTOR = {self.scale_factor}")
 
         ax = fig.add_subplot(1, 1, 1)
         ax.set_xlim(0, self.max_x_lim)
@@ -30,50 +30,45 @@ class EEGPlot:
 
         return ax
 
-    def create_figure(self, n_subplots, figsize): 
+    def create_figure(self, n_subplots, figsize):
         # n_subplots default it to 19 for EEG
         fig = Figure(figsize=figsize, dpi=100)
         axes = self.create_axes(n_subplots, fig)
         return fig, axes
 
+    def get_s_freq(self, raw_eeg):
+        return raw_eeg.info["sfreq"]
+
     def get_signal_length_seconds(self, raw_eeg):
-        s_freq = raw_eeg.info["sfreq"]
+        s_freq = self.get_s_freq(raw_eeg)
         signal_length = raw_eeg.get_data().shape[-1]
 
         return signal_length / s_freq
-    
-    def read_excel(self,excel_path:str,s_freq:int = 256):
-        """ Read eeg data stored in excel format, specifically made for imports from `Haleluya Hospital` 
-            - this files don't have information about the sampling frequency (so it is assumed)
-            - contain patient and other unneeded info.
-            - are already stored in `Bipolar montage`, thus no conversion required
-            - have fewer channels (17)
-            args:
-                - excel_path(str): abs path to the excel file
-                - s_freq(int): sampling_frequenct, defaults to 256
+
+    def read_excel(self, excel_path: str, s_freq: int = 256):
+        """Read eeg data stored in excel format, specifically made for imports from `Haleluya Hospital`
+        - this files don't have information about the sampling frequency (so it is assumed)
+        - contain patient and other unneeded info.
+        - are already stored in `Bipolar montage`, thus no conversion required
+        - have fewer channels (17)
+        args:
+            - excel_path(str): abs path to the excel file
+            - s_freq(int): sampling_frequenct, defaults to 256
         """
-        print("Debug `read_excel`")
         eeg_df = pd.read_excel(excel_path)
-        print(eeg_df.head())
         # clean patient and other info
         eeg_df = eeg_df.iloc[16:]
-        print("Cleaning patient data")
-        print(eeg_df.head())
         # get channel names
         channel_pair_names = eeg_df.columns.to_list()
         # calcuate signal duration
         signal_length_seconds = len(eeg_df) // s_freq
         # convert signal to MNE format for compatibility
         mne_info = mne.create_info(
-                ch_names=channel_pair_names,
-                sfreq=s_freq,
-                ch_types="eeg"
-                )
-        mne_data = mne.io.RawArray(eeg_df.values.T,info=mne_info,verbose=False)
+            ch_names=channel_pair_names, sfreq=s_freq, ch_types="eeg"
+        )
+        mne_data = mne.io.RawArray(eeg_df.values.T, info=mne_info, verbose=False)
 
-        return(
-                mne_data,signal_length_seconds
-                )
+        return (mne_data, signal_length_seconds)
 
     def read_edf(self, edf_path: str, differential_array):
         """Read raw EEG signal stored in .edf format"""
@@ -81,9 +76,11 @@ class EEGPlot:
 
         raw_eeg.filter(None, 30.0, verbose=False)
 
-        return self.raw_to_bipolar(
-            raw_eeg, differential_array
-        ), self.get_signal_length_seconds(raw_eeg)
+        return (
+            self.raw_to_bipolar(raw_eeg, differential_array),
+            self.get_signal_length_seconds(raw_eeg),
+            self.get_s_freq(raw_eeg),
+        )
 
     def read_eeg(self, edf_path: str, differential_array):
         """Read raw EEG signal stored in .eeg format"""
@@ -92,20 +89,37 @@ class EEGPlot:
         # add a low-pass filter
         raw_eeg.filter(None, 30.0, verbose=False)
 
-        return self.raw_to_bipolar(
-            raw_eeg, differential_array
-        ), self.get_signal_length_seconds(raw_eeg)
+        return (
+            self.raw_to_bipolar(raw_eeg, differential_array),
+            self.get_signal_length_seconds(raw_eeg),
+            self.get_s_freq(raw_eeg),
+        )
 
     def raw_to_bipolar(self, raw_eeg, channel_pair_dict):
         bipolar_diff = []
         channel_pair_names = []
-        # chanel_names = raw_eeg.info["ch_names"]
+
+        channel_names_lowercase = [ch.lower() for ch in raw_eeg.info["ch_names"]]
 
         for key, value in channel_pair_dict.items():
-            c_1 = raw_eeg[value[0].strip()][0][0].astype(np.float16)
-            c_2 = raw_eeg[value[1].strip()][0][0].astype(np.float16)
-            bipolar_diff.append(c_1 - c_2)
-            channel_pair_names.append(key)
+            channel_a = value[0].strip()
+            channel_b = value[1].strip()
+
+            if channel_a.lower() in channel_names_lowercase:
+                try:
+                    c_1 = raw_eeg[channel_a][0][0].astype(np.float16)
+                except ValueError:
+                    channel_a = channel_a.upper()
+                    c_1 = raw_eeg[channel_a][0][0].astype(np.float16)
+
+                try:
+                    c_2 = raw_eeg[channel_b][0][0].astype(np.float16)
+                except ValueError:
+                    channel_b = channel_b.upper()
+                    c_2 = raw_eeg[channel_b][0][0].astype(np.float16)
+
+                bipolar_diff.append(c_1 - c_2)
+                channel_pair_names.append(key)
 
         # create a mne eef data
         bipolar_montage_info = mne.create_info(
@@ -123,8 +137,6 @@ class EEGPlot:
         # give each axes a y-label corresponding to teh channel
         # or bipolar montage pair name
         self.y_tick_pos = [i * self.scale_factor for i in range(len(channel_names))]
-        print(f"{self.y_tick_pos=}")
-        print(f"{channel_names=}")
 
         axes.set_yticks(self.y_tick_pos)
         axes.set_yticklabels(channel_names)
